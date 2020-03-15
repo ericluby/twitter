8526f630f75bc0a8342314390f8127d603254322
ericluby <60050306+ericluby@users.noreply.github.com>
2020-03-15 18:02:49 -0400

    User can delete their Tweet

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser')
 
 server.use(bodyParser.json())
 
+//user can read tweets
 server.get('/users/:userId/tweets', async function requestHandler(req, res){
   const userId = req.params.userId;
   const tweetsByUser = await dbCxn.any(`
@@ -23,14 +24,35 @@ server.get('/users/:userId/tweets', async function requestHandler(req, res){
   res.status(200).json(tweetsByUser);
 });
 
+//these are all the same way to replace something as seen in the sql code
+  // `where author_id = ${userId}`
+  // 'where author_id = ${userId}'.replace('${userId}',userId)
+  // 'where author_id = $1'.replace('$1',userId)
+
+//user can write tweets
 server.post('/users/:userId/tweets', async function requestHandler(req, res){
   const userId = req.params.userId;
   const message = req.body.message;
   await dbCxn.any(`
-    INSERT INTO Tweets (author_id, message)
-    VALUES ($1, $2)
+    INSERT INTO
+      Tweets
+      (author_id, message)
+    VALUES
+      ($1, $2)
   `, [userId, message]);
    res.sendStatus(201)
+})
+
+//user can delete tweets
+server.delete('/users/:userId/tweets/:tweetId', async function requestHandler(req, res){
+  const tweetId = req.params.tweetId;
+  await dbCxn.any(`
+    DELETE FROM
+      Tweets
+    WHERE
+      id = $1
+  `, [tweetId]);
+  res.sendStatus(204)
 })
 
 server.listen(port, () => console.log(`Example app listening on port ${port}!`))
