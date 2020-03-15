97798200fa6a173ed3a5a8a95c4ec779206ce810
ericluby <60050306+ericluby@users.noreply.github.com>
2020-03-14 20:13:01 -0400

    user can create tweets via api

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const server = express()
 const port = process.env.PORT
 var pgp = require('pg-promise')(/* options */)
 var dbCxn = pgp(process.env.DATABASE_URL);
+var bodyParser = require('body-parser')
+
+server.use(bodyParser.json())
 
 server.get('/users/:userId/tweets', async function requestHandler(req, res){
   const userId = req.params.userId;
@@ -19,5 +22,15 @@ server.get('/users/:userId/tweets', async function requestHandler(req, res){
   `, [userId]);
   res.status(200).json(tweetsByUser);
 });
+
+server.post('/users/:userId/tweets', async function requestHandler(req, res){
+  const userId = req.params.userId;
+  const message = req.body.message;
+  await dbCxn.any(`
+    INSERT INTO Tweets (author_id, message)
+    VALUES ($1, $2)
+  `, [userId, message]);
+   res.sendStatus(201)
+})
 
 server.listen(port, () => console.log(`Example app listening on port ${port}!`))
