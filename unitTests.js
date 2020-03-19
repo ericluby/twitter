3bca4b49392ecd63c8e10a7cf686ce74e15f479e
ericluby <60050306+ericluby@users.noreply.github.com>
2020-03-19 18:39:46 -0400

    fix failing test

--- unitTests.js
+++ unitTests.js
@@ -3,7 +3,7 @@ const isValidPassword = require("./passwordUtils.js").isValidPassword
 
 test('When password + hash is valid', function (t) {
     t.plan(1);
-    t.equal(isValidPassword('berrybad', '$2a$08$5p9pB9/dKcKsxCM/53lGP.5P2beUCbrRu1dscP4iB0cJvnpznfXGS'), true);
+    t.equal(isValidPassword('berry', '$2a$08$5p9pB9/dKcKsxCM/53lGP.5P2beUCbrRu1dscP4iB0cJvnpznfXGS'), true);
     t.end()
 });
 
